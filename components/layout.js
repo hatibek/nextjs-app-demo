868c67226dd6cccfabfec0665f411573d27df4cb
hatibe <hatibekara@gmail.com>
2024-06-04 13:42:59 -0700

    updates bgcolor and photo size

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
 
 const name = "Hello, World!";
-export const siteTitle = "Next.js Sample Website";
+export const siteTitle = "Libellum Hatibei";
 
 export default function Layout({ children, home }) {
   return (
