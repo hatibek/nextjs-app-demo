f0fc783248b96320bd19bb87485fd06dc1081200
hatibe <hatibekara@gmail.com>
2024-06-06 09:32:36 -0700

    updates styles of the blog post list, adds 404 page

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import Head from "next/head";
+import Link from "next/link";
+import Date from "../components/date";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 
@@ -24,17 +26,16 @@ export default function Home({ allPostsData }) {
           My name is Hatibe. I am a software engineer, classicist, hiker and a
           chef.
         </p>
-        <p>LINKS</p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         {allPostsData.map(({ id, date, title }) => (
           <li className={utilStyles.listItem} key={id}>
-            {title}
+            <Link href={`/posts/${id}`}>{title}</Link>
             <br />
-            {id}
-            <br />
-            {date}
+            <small className={utilStyles.lightText}>
+              <Date dateString={date} />
+            </small>
           </li>
         ))}
       </section>
